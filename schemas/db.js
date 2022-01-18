608cf48f375c32e8aa0ac6340f5dbc0ab887bfbf
DarkPhoenixReborn <69182771+DarkPhoenixReborn@users.noreply.github.com>
2022-01-18 13:41:59 -0600

    Create db.js

---
+++ schemas/db.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const dbSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    guildID: String,
+    guildName: String,
+    memberRoles: [Object],
+    modRoles: [Object],
+    commands: [Object],
+    channels: [Object],
+    xpMultiplier: Number,
+    xpRoles: [Object],
+    reactionRoles: [Object],
+    reactionCommands: [Object],
+    xpSystem: Boolean,
+    profanityFilter: Boolean,
+    antiSpam: Boolean,
+    badWordList: [String],
+    welcomeMessage: [String],
+});
+
+module.exports = mongoose.model("db", dbSchema)
