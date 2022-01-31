e64325404fe85472141d367aca88d56932e336c7
DarkPhoenixReborn <69182771+DarkPhoenixReborn@users.noreply.github.com>
2022-01-31 11:41:22 -0600

    Delete sarcasm.js

--- commands/fun/sarcasm.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-        name: "sarcasm",
-        description: "sends a sarcastic comment",
-        category: "fun",
-        accessableby: "Members", 
-        run: async (bot, message, args) => {        
-        message.reply('Light travels faster than sound. This is why you appear bright until you start speaking');
-}
-   }
