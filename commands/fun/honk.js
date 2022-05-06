c15e2e0e313e32d3507e3cacfa711d3d9811bf1e
ArkanReborn <69182771+ArkanReborn@users.noreply.github.com>
2022-05-06 12:38:03 -0500

    Delete honk.js

--- commands/fun/honk.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: "honk",
-    description: "sends a funny duck meme",
-    category: "fun",
-    accessableby: "Members", 
-    run: async (bot, message, args) => {    
-        return message.reply(":duck: you mess with the honk, you get the bonk!")
-}
-}
