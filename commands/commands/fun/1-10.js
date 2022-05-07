778360215daee021574f9f0a4539c442dbf8cfad
ArkanReborn <69182771+ArkanReborn@users.noreply.github.com>
2022-05-07 11:18:00 -0500

    Delete commands/commands directory

--- commands/commands/fun/1-10.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: "1-10",
-    description: "sends a random number from 1-10",
-    category: "fun",
-    accessableby: "Members",
-    run: async (bot, message, args) => {
-        message.channel.send(Math.floor(Math.random() * 10));
-    }
-}
--- commands/commands/fun/hello.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: "hello",
-    description: "sends a basic greeting",
-    category: "fun",
-    accessableby: "Members",
-    run: async (bot, message, args) => {
-        return message.reply("Hello")
-    }
-}
